e87da5fa5640e7bd575c73914849fd7247ea7765
sagardesai90 <saagardesai@gmail.com>
2019-02-28 09:34:53 -0800

    First commit

---
+++ src/main/java/com/lambdaschool/javaourcourses/JavaOurcoursesApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.javaourcourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOurcoursesApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JavaOurcoursesApplication.class, args);
+    }
+
+}
