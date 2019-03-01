4fc7e9a1c1cbbaf2e9f4b3b8316b33715bccc362
sagardesai90 <saagardesai@gmail.com>
2019-03-01 08:47:32 -0800

    user controller

--- ourcourses/src/main/java/com/lambdaschool/ourcourses/controller/UserController.java
+++ ourcourses/src/main/java/com/lambdaschool/ourcourses/controller/UserController.java
@@ -1,37 +1,49 @@
-package com.lambdaschool.ourcourses.controller;
-
 import com.lambdaschool.ourcourses.model.User;
-import com.lambdaschool.ourcourses.service.UserService;
+import com.lambdaschool.ourcourses.repository.Userrepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-public class UserController
+public class Usercontroller
 {
 
     @Autowired
-    private UserService userService;
+    // private UserService userService;
+    private Userrepository userrepos;
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public List<User> listUser()
+    @GetMapping("/users")
+    public List<User> listAllUsers()
     {
-        return userService.findAll();
+        return userrepos.findAll();
     }
 
-    @RequestMapping(value = "/user", method = RequestMethod.POST)
-    public User create(@RequestBody User user)
+    @PostMapping("/user")
+    public User addNewUser(@RequestBody User newuser) throws URISyntaxException
     {
-        return userService.save(user);
+        return userrepos.save(newuser);
     }
 
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
-    public String delete(@PathVariable(value = "id") Long id)
+    @DeleteMapping("/users/{id}")
+    public String deleteUserById(@PathVariable long id)
     {
-        userService.delete(id);
-        return "success";
+        var foundUser = userrepos.findById(id);
+        if (foundUser.isPresent())
+        {
+            userrepos.deleteById(id);
+
+            return "{" + "\"id\":"   + foundUser.get().getId() +
+                    ",\"usename\":" + "\"" + foundUser.get().getUsername() + "\"" +
+                    ",\"role\":" + foundUser.get().getAuthority() + "}";
+        }
+        else
+        {
+            return null;
+        }
     }
 
 }
+Collap
